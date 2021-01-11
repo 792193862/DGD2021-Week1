b2719f7f52ca9365af85dba5bab1a3eee8bfc192
Bruno de Paula <brunohpaula@gmail.com>
2021-01-11 09:56:10 +0000

    Week 1 - Pre-Session
    Pre-Session Project Status

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text message1;
+    public Text message2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
